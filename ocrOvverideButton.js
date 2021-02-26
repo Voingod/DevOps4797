b714c416e2b12b12ea25cdc98f32218bd5fd4ec5
Bohdan Voinyk <bohdan.voinyk@mobexo.de>
2021-02-26 17:09:47 +0200

    bugfix

--- ocrOvverideButton.js
+++ ocrOvverideButton.js
@@ -1,4 +1,4 @@
-function OvverideDeacivateButtonOnHome(param1, param2, param3) {
+function OvverideDeacivateButtonOnHome(SelectedControlSelectedItemReferences) {
     let confirmStrings = {
         title: "Achtung: Sie deaktiveren den Vorgang!",
         text: "Damit wird das hinterlegte Dokument auch gelöscht und ist nicht mehr im Fahrzeug ersichtlich!",
@@ -8,20 +8,28 @@ function OvverideDeacivateButtonOnHome(param1, param2, param3) {
     Xrm.Navigation.openConfirmDialog(confirmStrings, null).then(
         function (success) {
             if (success.confirmed)
-                XrmCore.Commands.Deactivate.deactivateRecords(param1, param2, param3);
+                DeactivateOCR(SelectedControlSelectedItemReferences, null);
         });
 }
 
-function OvverideDeacivateButtonOnForm(param1, param2, ocrId) {
+function OvverideDeacivateButtonOnForm() {
 
     //100000000 - Irrläufer
     //100000004 - In Eingangsrechnung verschoben
     debugger;
+    let ocrId = Xrm.Page.data.entity.getId().replace('{', '').replace('}', '');
     let statusBearbeitung = Xrm.Page.getAttribute("new_status_bearbeitung").getValue();
-    // if (statusBearbeitung != 100000000 || statusBearbeitung != 100000004) {
-    //     //XrmCore.Commands.Deactivate.deactivatePrimaryRecord(param1, param2);
-    //     return;
-    // }
+    let data = { "Value": ocrId.replace('{', '').replace('}', '') };
+    //let entityStatusCode = { statuscode: 2, statecode: 1 };
+
+    let OCREntity = { entityType: "uds_document_recognition", id: Xrm.Page.data.entity.getId() };
+
+    if (statusBearbeitung != 100000000 && statusBearbeitung != 100000004) {
+        DeactivateOCR(null, OCREntity)
+        //CallAction("PATCH", entityStatusCode, "/uds_document_recognitions(" + ocrId + ")", true);
+        //parent.window.location.reload();
+        return;
+    }
     let fetch = '<fetch returntotalrecordcount="true">' +
         '<entity name="uds_document_recognition">' +
         '<link-entity name="uds_uds_document_recognition_new_fuhrpark" from="uds_document_recognitionid" to="uds_document_recognitionid" intersect="true">' +
@@ -32,14 +40,14 @@ function OvverideDeacivateButtonOnForm(param1, param2, ocrId) {
         '</entity>' +
         '</fetch>';
     let url = "/uds_document_recognitions?fetchXml=" + fetch;
-    let entity = Xrm.Page.data.entity;
-    //let result = getReleatedEntitiesWithFuhrpark("GET", url, null,false);
-    var data = { "Value": ocrId.replace('{', '').replace('}', '') };
+
     let result = CallAction("GET", data, url, false)
-    // if (result["@odata.count"] < 2) {
-    //     //XrmCore.Commands.Deactivate.deactivatePrimaryRecord(param1, param2);
-    //     return;
-    // }
+    if (result["@odata.count"] < 2) {
+        //CallAction("PATCH", entityStatusCode, "/uds_document_recognitions(" + ocrId + ")", true);
+        DeactivateOCR(null, OCREntity)
+        //parent.window.location.reload();
+        return;
+    }
 
     let confirmStrings = {
         title: "Achtung: Der Status wurde auf \"Irrläufer\" oder \"In Eingangsrechnung verschoben\" gesetzt!",
@@ -51,12 +59,11 @@ function OvverideDeacivateButtonOnForm(param1, param2, ocrId) {
     Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
         function (success) {
             if (success.confirmed) {
-                let deleteFilesActionUrl = "/uds_document_recognitions(" + ocrId.replace('{', '').replace('}', '') + ")/Microsoft.Dynamics.CRM.uds_DeleteFilesFromFuhrparkAction";
-                //getReleatedEntitiesWithFuhrpark("POST", deleteFilesActionUrl, data,true);
-                var data = { "Value": ocrId.replace('{', '').replace('}', '') };
-                CallAction("POST", data, deleteFilesActionUrl, true);
-                return;
-                XrmCore.Commands.Deactivate.deactivatePrimaryRecord(param1, param2);
+                //let deleteFilesActionUrl = "/uds_document_recognitions(" + ocrId + ")/Microsoft.Dynamics.CRM.uds_DeleteFilesFromFuhrparkAction";
+                //CallAction("POST", data, deleteFilesActionUrl, true);
+                //CallAction("PATCH", entityStatusCode, "/uds_document_recognitions(" + ocrId + ")", true);
+                DeactivateOCR(null, OCREntity)
+                //parent.window.location.reload();
             }
         });
 }
@@ -64,10 +71,9 @@ function OvverideDeacivateButtonOnForm(param1, param2, ocrId) {
 function CallAction(method, dataPass, url, sync) {
     const version = window.parent.Xrm.Page.context.getVersion();
     const apiUrl = window.parent.Xrm.Page.context.getClientUrl() + "/api/data/v" + version.slice(0, version.indexOf(".") + 2);
-    
-
+    Xrm.Utility.showProgressIndicator('Verarbeiten...');
     let req = new XMLHttpRequest();
-    let data ={};
+    let data = {};
     req.open(method, apiUrl + url, sync);
 
     req.setRequestHeader("Accept", "application/json");
@@ -76,18 +82,75 @@ function CallAction(method, dataPass, url, sync) {
     req.setRequestHeader("OData-Version", "4.0");
     req.onreadystatechange = function () {
         if (this.readyState == 4) {
+            parent.window.Xrm.Utility.closeProgressIndicator();
             req.onreadystatechange = null;
-            if (this.status == 200 || this.status == 204) {
+            if (this.status == 200) {
                 data = JSON.parse(this.response);
-                console.log(data);   
+                console.log(data);
             }
-            else{
-                Xrm.Navigation.openAlertDialog(error.message)
-
+            else if (this.status == 204) {
+            }
+            else {
+                Xrm.Navigation.openAlertDialog(this.message)
             }
         }
     };
 
     req.send(window.JSON.stringify(dataPass));
     return data;
+}
+
+
+function DeactivateOCR(SelectedControlSelectedItemReferences, OCREntity) {
+    Xrm.Utility.showProgressIndicator('Verarbeiten...');
+    let Sdk = window.Sdk || {};
+    Sdk.Deactivate = function (OCRCollections, OCREntity) {
+        this.OCRCollections = OCRCollections;
+        this.OCREntity = OCREntity;
+    };
+
+    Sdk.Deactivate.prototype.getMetadata = function () {
+        return {
+            boundParameter: null,
+            parameterTypes: {
+                "OCRCollections": { // entity collection
+                    typeName: "Collection(mscrm.crmbaseentity)",
+                    structuralProperty: 4 // collection type
+                },
+                "OCREntity": { // entity
+                    typeName: "mscrm.uds_document_recognition",
+                    structuralProperty: 5
+                }
+            },
+            operationType: 0,
+            operationName: "uds_DeleteFilesFromFuhrparkAction"
+        }
+    };
+    let arr = [];
+    let deactivateRequest;
+    if (SelectedControlSelectedItemReferences != null) {
+        for (let i = 0; i < SelectedControlSelectedItemReferences.length; i++) {
+            let obj = {};
+            obj.entityType = SelectedControlSelectedItemReferences[i].TypeName;
+            obj.id = SelectedControlSelectedItemReferences[i].Id;
+            arr.push(obj);
+        }
+        deactivateRequest = new Sdk.Deactivate(arr, OCREntity)
+    }
+    else {
+        deactivateRequest = new Sdk.Deactivate(null, OCREntity)
+    }
+
+    Xrm.WebApi.online.execute(deactivateRequest).then(
+        function (response) {
+            parent.window.Xrm.Utility.closeProgressIndicator();
+            if (response.ok) {
+                parent.window.location.reload();
+            }
+        },
+        function (error) {
+            parent.window.Xrm.Utility.closeProgressIndicator();
+            console.log(error.message);
+        }
+    );
 }
